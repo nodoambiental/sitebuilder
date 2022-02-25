3f8da321a9fb5265247b0428667ec12ca4c6a3c3
Ágata Ordano <ordanoagata@gmail.com>
2022-02-25 16:18:26 -0300

    feat(pug): add theme changer button in navbar

--- src/bundle.ts
+++ src/bundle.ts
@@ -1,8 +1,15 @@
+// Tailwind/DaisyUI
 import "./SCSS/input.scss";
+
+// Whatever logic is involved
 import "./templates";
 
+// Pug templates
 const requireAll = (requirement: any) => {
     requirement.keys().forEach(requirement);
 };
-
 requireAll(require.context("./templates/", true, /\.pug$/));
+
+// Themes
+const { themeChange } = require("theme-change");
+themeChange();
