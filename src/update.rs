92b45b1ccbbd7a5fbdf059e5f0a7ec5d21478b97
Ágata Ordano <ordanoagata@gmail.com>
2022-03-20 20:19:30 -0300

    feat(update): add dependencies installer handler

--- src/update.rs
+++ src/update.rs
@@ -1,9 +1,16 @@
 use super::util;
 use git2;
+use std::{fs, io, process};
 
-pub fn update(flags: [&str; 5]) {
+pub fn update(sub_match: &clap::ArgMatches) {
     // TODO
-    // Actual function that reads flags present and calls the proper methods
+    if sub_match.is_present("install") {
+        node_install();
+    }
+    let content = sub_match.is_present("content");
+    let source = sub_match.is_present("source");
+    let assets = sub_match.is_present("assets");
+    let data = sub_match.is_present("data");
 }
 
 pub fn pull(repo_uri: &str) {
@@ -16,7 +23,28 @@ pub fn clone(repo_uri: &str) {
     // Function that verifiers that the folders are not present and clones them
 }
 
-pub fn node_install() {
-    // TODO
-    // Function that performs the node_modules dependencies installation
+pub fn node_install() -> Result<std::process::ExitStatus, std::io::Error> {
+    // Verify the `source` directory exists
+    let source_exists = util::verify_reldir("source").unwrap();
+    assert!(
+        matches!(source_exists, true),
+        "[FS] Source folder does not exist"
+    );
+
+    // Verify package.json exists
+    let package_exists = util::verify_relfile("source", "package", "json").unwrap();
+    assert!(
+        matches!(package_exists, true),
+        "[FS] There is no package.json file in the source folder"
+    );
+
+    // Install modules
+    let code = process::Command::new("npm")
+        .arg("install")
+        .current_dir(util::cwd_string() + "/source")
+        .spawn()
+        .expect("[NPM] ")
+        .wait();
+
+    return code;
 }
--- src/util.rs
+++ src/util.rs
@@ -1,17 +1,16 @@
 use config;
-use std::env;
-use std::fs;
-use std::io;
-use std::path;
+use std::{env, fs, io, path};
 
-pub fn create_reldir(rel_path: &str) -> Result<(), io::Error> {
-    let cwd = env::current_dir()
+pub fn cwd_string() -> String {
+    env::current_dir()
         .unwrap()
         .into_os_string()
         .into_string()
-        .unwrap();
+        .unwrap()
+}
 
-    let temp_path: path::PathBuf = [cwd, String::from(rel_path)].iter().collect();
+pub fn create_reldir(rel_path: &str) -> Result<(), io::Error> {
+    let temp_path: path::PathBuf = [cwd_string(), String::from(rel_path)].iter().collect();
 
     fs::create_dir_all(temp_path).unwrap();
 
@@ -20,18 +19,28 @@ pub fn create_reldir(rel_path: &str) -> Result<(), io::Error> {
 }
 
 pub fn delete_reldir(rel_path: &str) -> Result<(), io::Error> {
-    let cwd = env::current_dir()
-        .unwrap()
-        .into_os_string()
-        .into_string()
-        .unwrap();
-
-    let temp_path: path::PathBuf = [cwd, String::from(rel_path)].iter().collect();
-
+    let temp_path: path::PathBuf = [cwd_string(), String::from(rel_path)].iter().collect();
     fs::remove_dir(temp_path).unwrap();
 
     // HACK add error handling
     Ok(())
+}
+
+pub fn verify_reldir(rel_path: &str) -> io::Result<bool> {
+    let mut path = env::current_dir()?;
+    path.push(rel_path);
+    let metadata = fs::metadata(path)?;
+    Ok(metadata.is_dir())
+}
+
+pub fn verify_relfile(rel_path: &str, name: &str, ext: &str) -> io::Result<bool> {
+    let mut fullpath = path::PathBuf::new();
+    fullpath.push(cwd_string());
+    fullpath.push(rel_path);
+    fullpath.push(name);
+    fullpath.set_extension(ext);
+    let metadata = fs::metadata(fullpath.to_str().unwrap())?;
+    Ok(metadata.is_file())
 }
 
 pub fn read_config(config_path: &str) -> Result<(), io::Error> {
